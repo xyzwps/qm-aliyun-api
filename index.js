17b0639c6c99a0090aa7c6bb934ae3c474503232
亮玉 韦 <weily@yingmi.cn>
2019-03-15 14:37:57 +0800

    cdn push object cache

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const { generateToken } = require('./lib/utils');
+const cdn = require('./lib/cdn');
 
 module.exports = {
-  generateApiToken: generateToken
+  generateApiToken: generateToken,
+  cdn
 };
