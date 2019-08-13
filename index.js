ef7f08ccb8b072f72c8a921bb63481a3c2c8733f
亮玉 韦 <weily@yingmi.cn>
2019-08-13 10:11:38 +0800

    支持发送短信

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const { generateToken } = require('./lib/utils');
 const cdn = require('./lib/cdn');
 const push = require('./lib/push');
 const CloudPush = require('./lib/apis/CloudPush');
+const Sms = require('./lib/apis/Sms');
 const VoiceCall = require('./lib/apis/VoiceCall');
 
 module.exports = {
   generateApiToken: generateToken,
   cdn,
   push,
+  Sms,
   VoiceCall,
   CloudPush
 };
